ec4b51de8ded12f926351704c3efcdd087a13515
Sahil <2016ucs0008@iitjammu.ac.in>
2020-01-19 12:14:41 +0530

    Add 2 problems

---
+++ 3/q0/solution.cpp
@@ -0,0 +1,10 @@
+#include "hello_world.h"
+
+using namespace std;
+
+// You must return the string "Hello, World!"
+string hello() {
+    // Return the string we need.
+    return "";
+}
+
---
+++ 3/q1/solution.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "pointer.h"
+
+using namespace std;
+
+// you are given a pointer to the first element of the array
+// return the sum of all elements in the array
+int sum_array(int *arr, int N) {
+	// write your code here
+}
